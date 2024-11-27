12b43241424fd101af142501b8f14dc72dd98411
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-11-28 00:21:29 +0530

    environment variables created for firebase credentials

--- firebase/config.ts
+++ firebase/config.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDA3x3zxzzQtKDZUspujQMfO3F1PxQCskg",
-  authDomain: "notekeeping-e82b6.firebaseapp.com",
-  projectId: "notekeeping-e82b6",
-  storageBucket: "notekeeping-e82b6.firebasestorage.app",
-  messagingSenderId: "968641296270",
-  appId: "1:968641296270:web:5205d7cbca48ab5875d6df",
-  measurementId: "G-59QHNEK6W0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
